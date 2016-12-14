f9a6037766aa9251adcde86593ef0e7e70d784f9
Søren Sønderby Nielsen <ssn@e-conomic.com>
2016-12-14 17:30:07 +0100

    Removing pods from git

--- Pods/Target Support Files/Pods-ReSwiftTest/Pods-ReSwiftTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReSwiftTest : NSObject
-@end
-@implementation PodsDummy_Pods_ReSwiftTest
-@end
--- Pods/Target Support Files/ReSwift/ReSwift-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double ReSwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char ReSwiftVersionString[];
-
