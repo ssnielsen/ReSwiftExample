7356942f9062d48e9cb44cf58909168fade7af26
Søren Sønderby Nielsen <ssn@e-conomic.com>
2016-12-23 12:27:21 +0100

    WIP Add customer

---
+++ ReSwiftTest/NewCustomerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewCustomerViewController.swift
+//  ReSwiftTest
+//
+//  Created by Søren Nielsen on 22/12/2016.
+//  Copyright © 2016 e-conomic International A/S. All rights reserved.
+//
+
+import Foundation
